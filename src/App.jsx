cb46acbd92d25e6e976cbd4beb96d8315608cd99
Gorkem365 <pervergorkem09@gmail.com>
2025-11-09 13:41:55 +0300

    Initial commit: Proje kurulumu ve Tailwind başarıyla tamamlandı

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+﻿function App() {
+    return (
+        <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+            <h1 className="text-5xl font-bold text-cyan-400">
+                Tailwind Kurulumu Başarılı!
+            </h1>
+        </div>
+    )
+}
+
+export default App
